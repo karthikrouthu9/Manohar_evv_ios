2e5f58e3a9aae60a25033011f4d237f6625cc409
karthikrouthu9 <routhukarthik93@gmail.com>
2015-11-25 02:33:22 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -46,9 +46,11 @@ function fail(uuid)
     
 		function onDeviceReady() {
 			alert("Device Ready");
-		GPSLocation.getCurrentPosition(geolocationSuccess,
-                                         [geolocationError],
-                                         [geolocationOptions]);
+	cordova.plugins.diagnostic.isLocationEnabled(function(enabled){
+    console.log("Location is " + (enabled ? "enabled" : "disabled"));
+}, function(error){
+    console.error("The following error occurred: "+error);
+});
                        window.plugins.uniqueDeviceID.get(success, fail);
     }
     function index_page(){
@@ -205,22 +207,4 @@ function fail(uuid)
 }
     			
 
-
-var onSuccess = function(position) {
-    alert('Latitude: '          + position.coords.latitude          + '\n' +
-          'Longitude: '         + position.coords.longitude         + '\n' +
-          'Altitude: '          + position.coords.altitude          + '\n' +
-          'Accuracy: '          + position.coords.accuracy          + '\n' +
-          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-          'Heading: '           + position.coords.heading           + '\n' +
-          'Speed: '             + position.coords.speed             + '\n' +
-          'Timestamp: '         + position.timestamp                + '\n');
-};
-
-// onError Callback receives a PositionError object
-//
-function onError(error) {
-    alert('code: '    + error.code    + '\n' +
-          'message: ' + error.message + '\n');
-}
 
